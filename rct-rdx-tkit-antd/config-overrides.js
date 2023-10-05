7547419113121f4f7213ddae2fba346d4d1421fc
Priyanath Mukherjee <m.priyanath.m@gmail.com>
2023-10-05 18:01:46 +0530

    .less plugin func added with customized-cra

---
+++ rct-rdx-tkit-antd/config-overrides.js
@@ -0,0 +1,23 @@
+const lessRegex = /\.less$/;
+const lessModuleRegex = /\.module\.less$/;
+
+const { override, fixBabelImports, addLessLoader, addWebpackPlugin } = require('customize-cra');
+const AntdDayJsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+module.exports = override(
+    fixBabelImports('antd', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
+        lessOptions: {
+            javascriptEnabled: true,
+            modifyVars: {
+                '@primary-color': '#1DA57A',
+            }
+        }
+    }),
+    // addWebpackPlugin(
+    //     new AntdDayJsWebpackPlugin()
+    // )
+)
